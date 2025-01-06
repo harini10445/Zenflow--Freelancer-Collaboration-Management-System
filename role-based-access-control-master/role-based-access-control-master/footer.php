505abca9f84ec30442a455a8ac48a26baa749e16
harini10445 <111628045+harini10445@users.noreply.github.com>
2025-01-06 14:34:00 +0530

    Add files via upload

---
+++ role-based-access-control-master/role-based-access-control-master/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="panel panel-default">
+        <div class="panel-body small">
+            <span>Copyright &copy; <?= (date('Y') !== '2017' ? '2017-' : '') . date('Y') ?> <a href="https://github.com/hamza1886" target="_blank">Hamza Rashid</a>. All rights reserved.</span>
+
+            <span class="pull-right"><a href="https://github.com/hamza1886/role-based-access-control#license" target="_blank">License</a></span>
+        </div>
+    </div>
+</footer>
